ce8cd47ca8f3c62833061bc6ac7855bee4fcebb8
Luhao0906 <2777096@learn.vsb.bc.ca>
2026-09-14 13:34:19 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -1,1 +1,12 @@
+public class Time{
+	
+	public static void main(String[] args){
+			int hour = 13;
+			int minute = 20;
+			int second = 27;
+			int midnight = ((13*3600)+(20*60)+27);
+			System.out.print(midnight);
+			
+		}
+}
 

f399055c9945a3999bad6db50e5d4a54dc1e72ec
luhao <2777096@learn.vsb.bc.ca>
2026-09-15 13:02:45 -0700

    Add files via upload

--- date.java
+++ date.java
@@ -1,10 +1,11 @@
 public class date{
 	
 	public static void main(String[] args){
-			String day = "Monday";
+			String day = " Monday ";
 			int date = 14;
-			String month = "September";
+			String month = " September ";
 			int year = 2026;
 			System.out.println(day + "," + month + date + "," + year);
+			System.out.println(day + date + month +year);
 		}
 }

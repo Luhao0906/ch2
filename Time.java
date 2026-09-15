67ff97be639b4a61295e623211ee1fe7405f5b8b
luhao <2777096@learn.vsb.bc.ca>
2026-09-15 12:54:05 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -2,10 +2,20 @@ public class Time{
 	
 	public static void main(String[] args){
 			int hour = 13;
-			int minute = 20;
-			int second = 27;
-			int midnight = ((13*3600)+(20*60)+27);
-			System.out.print(midnight);
+			int minute = 34;
+			int second = 35;
+			int minutea = 20;
+			int seconda = 27;
+			double midnight = ((13*3600)+(20*60)+27);
+			int day = 24*60*60;
+			double remainingday = day - midnight;
+			double percent = midnight / day * 100;
+			int passtime = (34*60+35) - (20*60+27);
+			System.out.println(midnight);
+			System.out.println(remainingday);
+			System.out.print(percent);
+			System.out.println("%");
+			System.out.println(passtime);
 			
 		}
 }
